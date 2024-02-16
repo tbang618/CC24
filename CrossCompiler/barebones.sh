0b6e3bbf0850772282eedf1237cfc7b3bf46b7ad
Theodore <140735901+tbang618@users.noreply.github.com>
2024-02-16 00:24:34 -0600

    Add files via upload

---
+++ CrossCompiler/barebones.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# For creating a cross-compiler toolchain
+# targeting: i686-elf
+#
+# first, make sure you created $HOME/opt/cross/
+# and that source files for GCC are at $HOME/src/gcc-13.2.0/
+# and that source files for Binutils are ate $HOME/src/binutils-2.42/
+#
+
+### Set-Up Environment for Build ###
+export PREFIX="$HOME/opt/cross"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+
+### Build Binutils ###
+cd $HOME/src
+
+mkdir build-binutils
+cd build-binutils
+../binutils-2.42/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+
+### Build GDB ###
+#../gdb.x.y.z/configure --target=$TARGET --prefix="$PREFIX" --disable-werror
+#make all-gdb
+#make install-gdb
+
+### Build GCC ###
+cd $HOME/src
+
+# Make sure $PREFIX/bin is in the $PATH
+which -- $TARGET-as || echo $TARGET-as is not in the PATH
+
+mkdir build-gcc
+cd build-gcc
+../gcc-13.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc
+
