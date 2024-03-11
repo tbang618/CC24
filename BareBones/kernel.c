d196fdc6e9ab766e89f3df0f0184468d17204021
Theodore <140735901+tbang618@users.noreply.github.com>
2024-03-11 18:33:51 -0500

    Fix kernel.c and add Makefile

--- BareBones/kernel.c
+++ BareBones/kernel.c
@@ -12,20 +12,22 @@
 
 /* Hardware text mode color constants */
 enum vga_color {
-   VGA_COLOR_BLACK = 0,
-   VGA_COLOR_BLUE = 1,
-   VGA_COLOR_GREEN = 2,
-   VGA_COLOR_CYAN = 3,
-   VGA_COLOR_RED = 4,
-   VGA_COLOR_MAGENTA = 5,
-   VGA_COLOR_BROWN = 6,
-   VGA_COLOR_LIGHT_GREY = 8,
-   VGA_COLOR_LIGHT_GREEN = 10,
-   VGA_COLOR_LIGHT_CYAN = 11,
-   VGA_COLOR_LIGHT_RED = 12,
-   VGA_COLOR_LIGHT_MAGENTA = 13,
-   VGA_COLOR_LIGHT_BROWN = 14,
-   VGA_COLOR_WHITE = 15,
+    VGA_COLOR_BLACK = 0,
+    VGA_COLOR_BLUE = 1,
+    VGA_COLOR_GREEN = 2,
+    VGA_COLOR_CYAN = 3,
+    VGA_COLOR_RED = 4,
+    VGA_COLOR_MAGENTA = 5,
+    VGA_COLOR_BROWN = 6,
+    VGA_COLOR_LIGHT_GREY = 7,
+    VGA_COLOR_DARK_GREY = 8,
+    VGA_COLOR_LIGHT_BLUE = 9,
+    VGA_COLOR_LIGHT_GREEN = 10,
+    VGA_COLOR_LIGHT_CYAN = 11,
+    VGA_COLOR_LIGHT_RED = 12,
+    VGA_COLOR_LIGHT_MAGENTA = 13,
+    VGA_COLOR_LIGHT_BROWN = 14,
+    VGA_COLOR_WHITE = 15,
 };
 
 /* Get number that defines the color of terminal */
@@ -36,11 +38,11 @@ static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
 
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
-    return (uint16_t) uc | (uint16_t) color << 8;
+    return (uint16_t)uc | (uint16_t)color << 8;
 }
 
 /* Get length of a string `str` */
-size_t strlen(const char* str)
+size_t strlen(const char *str)
 {
     size_t len = 0;
     while (str[len])
@@ -55,24 +57,25 @@ static const size_t VGA_HEIGHT = 25;
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
-uint16_t* terminal_buffer;
+uint16_t *terminal_buffer;
 
 void terminal_initialize(void)
 {
     terminal_row = 0;
     terminal_column = 0;
-    terminal_color = vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
-    terminal_buffer = (uint16_t*) 0xB8000; /* address */
-    for (size_t y = 0; y < VGA_HEIGHT; y++) {
-        for (size_t x = 0; x < VGA_WIDTH; y++) {
+    terminal_color = vga_entry_color(VGA_COLOR_RED, VGA_COLOR_WHITE);
+    terminal_buffer = (uint16_t *)0xB8000; 
+    for (size_t y = 0; y < VGA_HEIGHT; y++)
+    {
+        for (size_t x = 0; x < VGA_WIDTH; x++)
+        {
             const size_t index = y * VGA_WIDTH + x;
             terminal_buffer[index] = vga_entry(' ', terminal_color);
         }
     }
 }
 
-
-void terminal_setcolor(uint8_t color) 
+void terminal_setcolor(uint8_t color)
 {
     terminal_color = color;
 }
@@ -88,7 +91,8 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 void terminal_putchar(char c)
 {
     terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-    if (++terminal_column == VGA_WIDTH) {
+    if (++terminal_column == VGA_WIDTH)
+    {
         terminal_column = 0;
         if (++terminal_row == VGA_HEIGHT)
             terminal_row = 0;
@@ -96,19 +100,19 @@ void terminal_putchar(char c)
 }
 
 /* Write characters to the terminal */
-void terminal_write(const char* data, size_t size)
+void terminal_write(const char *data, size_t size)
 {
     for (size_t i = 0; i < size; i++)
         terminal_putchar(data[i]);
 }
 
 /* Write a string to the  */
-void terminal_writestring(const char* data)
+void terminal_writestring(const char *data)
 {
     terminal_write(data, strlen(data));
 }
 
-void kernel_main(void) 
+void kernel_main(void)
 {
     terminal_initialize();
 
